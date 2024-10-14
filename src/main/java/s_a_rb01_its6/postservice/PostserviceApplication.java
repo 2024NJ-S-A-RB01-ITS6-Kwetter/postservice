1f459ab80fbb2155611cff6f0b8edcb2a67619f0
Björn Wildeman <bwildeman99@gmail.com>
2024-10-14 02:21:05 +0200

    first commit

---
+++ src/main/java/s_a_rb01_its6/postservice/PostserviceApplication.java
@@ -0,0 +1,13 @@
+package s_a_rb01_its6.postservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s_a_rb01_its6/postservice/PostserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package s_a_rb01_its6.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
