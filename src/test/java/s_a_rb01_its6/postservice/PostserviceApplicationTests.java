76b061d245307097b32ba5ac3dd85854adb9e17e
Björn Wildeman <bwildeman99@gmail.com>
2024-12-11 23:15:31 +0100

    Test profile

--- src/test/java/s_a_rb01_its6/postservice/PostserviceApplicationTests.java
+++ src/test/java/s_a_rb01_its6/postservice/PostserviceApplicationTests.java
@@ -2,8 +2,10 @@ package s_a_rb01_its6.postservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PostserviceApplicationTests {
 
 	@Test
