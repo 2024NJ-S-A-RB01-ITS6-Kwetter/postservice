29d75e1077c5144aeabc1a226f9fb4a5dfb4ca63
Björn Wildeman <bwildeman99@gmail.com>
2025-01-16 15:42:14 +0100

    Async added

--- src/main/java/s_a_rb01_its6/postservice/controller/PostController.java
+++ src/main/java/s_a_rb01_its6/postservice/controller/PostController.java
@@ -23,6 +23,7 @@ import s_a_rb01_its6.postservice.service.exception.UnauthorizedDataAccessExcepti
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 import static s_a_rb01_its6.postservice.config.Constant.API_URL;
 
@@ -39,101 +40,91 @@ public class PostController {
 
     private final PostService postService;
 
-    //create post
-    //TODO make sure that user is posting to own account
+    // Create post (async)
     @PostMapping("/create")
-    public ResponseEntity<CreatePostResponse> createPost(@RequestBody CreatePostRequest createPostRequest) {
-        // Get authentication details
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public CompletableFuture<ResponseEntity<CreatePostResponse>> createPost(@RequestBody CreatePostRequest createPostRequest) {
+        return CompletableFuture.supplyAsync(() -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+            String userId = jwt.getClaimAsString("sub");
+            String preferredUsername = authentication.getName();
 
-        // Extract the 'sub' (user ID) from the JWT and use authentication.getName() for preferred_username
-        Jwt jwt = (Jwt) authentication.getPrincipal();
-        String userId = jwt.getClaimAsString("sub"); // User ID from JWT
-        String preferredUsername = authentication.getName(); // Preferred username directly from authentication
-
-        // Proceed with creating the post
-        CreatePostResponse createPostResponse = postService.createPost(createPostRequest, userId, preferredUsername);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createPostResponse);
+            CreatePostResponse createPostResponse = postService.createPost(createPostRequest, userId, preferredUsername);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createPostResponse);
+        });
     }
 
-    //get posts feed
-    //TODO MAKE PAGED
-    //TODO make this when friendservice is implemented
+    // Get posts feed (async)
     @GetMapping("/feed")
-    public ResponseEntity<String> getPostsFeed() {
-
-        return ResponseEntity.ok("Posts feed");
+    public CompletableFuture<ResponseEntity<String>> getPostsFeed() {
+        return CompletableFuture.supplyAsync(() -> ResponseEntity.ok("Posts feed"));
     }
 
-    //get posts on user profile
-    //TODO limit visibility of posts to only friends
+    // Get posts on user profile (async)
     @GetMapping("/profile/{username}")
-    public ResponseEntity<Map<String, Object>> getPostsOnUserProfile(@PathVariable String username,
+    public CompletableFuture<ResponseEntity<Map<String, Object>>> getPostsOnUserProfile(
+            @PathVariable String username,
             @RequestParam(name = "page", defaultValue = "1") int page,
             @RequestParam(name = "size", defaultValue = "10") int size) {
-        System.out.println("username: " + username);
-        Page<IndividualPost> posts = postService.getPostsOneUser(new GetPostsRequest(page, size, username));
-        Map<String, Object> response = new HashMap<>();
-        response.put(CURRENT_PAGE, posts.getNumber());
-        response.put(TOTAL_PAGES, posts.getTotalPages());
-        response.put(PAGE_SIZE, posts.getSize());
-        response.put(TOTAL_ELEMENTS, posts.getTotalElements());
-        response.put(POSTS, posts.getContent());
+        return CompletableFuture.supplyAsync(() -> {
+            Page<IndividualPost> posts = postService.getPostsOneUser(new GetPostsRequest(page, size, username));
+            Map<String, Object> response = new HashMap<>();
+            response.put(CURRENT_PAGE, posts.getNumber());
+            response.put(TOTAL_PAGES, posts.getTotalPages());
+            response.put(PAGE_SIZE, posts.getSize());
+            response.put(TOTAL_ELEMENTS, posts.getTotalElements());
+            response.put(POSTS, posts.getContent());
 
-        return ResponseEntity.ok(response);
+            return ResponseEntity.ok(response);
+        });
     }
 
-    //get post single
-    //TODO limit visibility of posts to only friends
+    // Get single post (async)
     @GetMapping("/{postId}")
-    public ResponseEntity<IndividualPost> getPostSingle(@PathVariable Long postId) {
-        IndividualPost response =postService.getPost(postId);
-        return ResponseEntity.ok(response);
+    public CompletableFuture<ResponseEntity<IndividualPost>> getPostSingle(@PathVariable Long postId) {
+        return CompletableFuture.supplyAsync(() -> {
+            IndividualPost response = postService.getPost(postId);
+            return ResponseEntity.ok(response);
+        });
     }
 
-
-    //delete post
+    // Delete post (async)
     @DeleteMapping("/delete")
-    public ResponseEntity<DeletePostResponse> deletePost(@RequestBody DeletePostRequest deletePostRequest) {
-        // Get authentication details
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public CompletableFuture<ResponseEntity<DeletePostResponse>> deletePost(@RequestBody DeletePostRequest deletePostRequest) {
+        return CompletableFuture.supplyAsync(() -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+            String userId = jwt.getClaimAsString("sub");
+            boolean isAdmin = authentication.getAuthorities().stream()
+                    .anyMatch(authority -> authority.getAuthority().equals("ROLE_ADMIN"));
 
-        // Extract the 'sub' (user ID) from the JWT and use authentication.getName() for preferred_username
-        Jwt jwt = (Jwt) authentication.getPrincipal();
-        String userId = jwt.getClaimAsString("sub");
-        //check if user is the author of the post or admin
-        boolean isAdmin = authentication.getAuthorities().stream()
-                .anyMatch(authority -> authority.getAuthority().equals("ROLE_ADMIN"));
+            if (!userId.equals(deletePostRequest.getAuthorId()) && !isAdmin) {
+                throw new UnauthorizedDataAccessException("You are not authorized to delete this post");
+            }
 
-        if (!userId.equals(deletePostRequest.getAuthorId()) && !isAdmin) {
-            throw new UnauthorizedDataAccessException("You are not authorized to delete this post");
-        }
-        DeletePostResponse deletePostResponse = postService.deletePost(deletePostRequest.getPostId());
-
-        return ResponseEntity.ok(deletePostResponse);
+            DeletePostResponse deletePostResponse = postService.deletePost(deletePostRequest.getPostId());
+            return ResponseEntity.ok(deletePostResponse);
+        });
     }
 
-
-    //search post
-    //TODO limit visibility of posts to only friends
+    // Search posts (async)
     @GetMapping("/search")
-    public ResponseEntity<Map<String, Object>> searchPost(
+    public CompletableFuture<ResponseEntity<Map<String, Object>>> searchPost(
             @RequestParam String query,
             @RequestParam(name = "page", defaultValue = "1") int page,
-            @RequestParam(name = "size", defaultValue = "10") int size
-    ) {
-        SearchPostRequest searchPostRequest = new SearchPostRequest(page, size, query);
-        Page<IndividualPost> posts = postService.searchPosts(searchPostRequest);
-        Map<String, Object> response = new HashMap<>();
-        response.put(CURRENT_PAGE, posts.getNumber());
-        response.put(TOTAL_PAGES, posts.getTotalPages());
-        response.put(PAGE_SIZE, posts.getSize());
-        response.put(TOTAL_ELEMENTS, posts.getTotalElements());
-        response.put(POSTS, posts.getContent());
+            @RequestParam(name = "size", defaultValue = "10") int size) {
+        return CompletableFuture.supplyAsync(() -> {
+            SearchPostRequest searchPostRequest = new SearchPostRequest(page, size, query);
+            Page<IndividualPost> posts = postService.searchPosts(searchPostRequest);
+            Map<String, Object> response = new HashMap<>();
+            response.put(CURRENT_PAGE, posts.getNumber());
+            response.put(TOTAL_PAGES, posts.getTotalPages());
+            response.put(PAGE_SIZE, posts.getSize());
+            response.put(TOTAL_ELEMENTS, posts.getTotalElements());
+            response.put(POSTS, posts.getContent());
 
-        return ResponseEntity.ok(response);
-
-
+            return ResponseEntity.ok(response);
+        });
     }
 
 
