7c01a7fb316cd32caa89d550a580fbf457086996
Björn Wildeman <bwildeman99@gmail.com>
2025-01-19 21:07:24 +0100

    Bad Words Implementation filter azure functions

--- src/main/java/s_a_rb01_its6/postservice/config/AppConfig.java
+++ src/main/java/s_a_rb01_its6/postservice/config/AppConfig.java
@@ -4,12 +4,19 @@ package s_a_rb01_its6.postservice.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class AppConfig {
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        // Customize the RestTemplate
+        return builder
+                .additionalMessageConverters(new StringHttpMessageConverter(StandardCharsets.UTF_8))
+                .build();
     }
 }
